5549279daede558ca58fc3675c8158e709ef1d24
HarryPotato986 <gdude986@gmail.com>
2025-12-19 23:18:25 -0500

    simple web design, video download works, no playlist or audio support yet.

--- VideoDownloader.py
+++ VideoDownloader.py
@@ -1,63 +1,93 @@
 from pytubefix import YouTube, Playlist
 import ffmpeg
+import os
 
 
-def Download(link="https://www.youtube.com/watch?v=OpelfZhK2N8",highResolution = False, SAVE_PATH = "videos"):
+def Download(link="https://www.youtube.com/watch?v=OpelfZhK2N8",resolution="worst", bitrate="worst", save_path = "videos"):
 
     #SAVE_PATH = "videos" #where it saves
 
     #link = "https://www.youtube.com/watch?v=OpelfZhK2N8"
 
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+
+    temp_path = save_path + "/temp"
+    if not os.path.isdir(temp_path):
+        os.mkdir(temp_path)
+
     try:
         yt = YouTube(link) #load video
     except:
-        print("Connection Error") #to handle exception
-        return False
+        print("YouTube Connection Error!!!") #to handle exception
+        return ["Can't Connect to YouTube!!! Please try again."]
 
-    if highResolution:
-        #stream = yt.streams.get_highest_resolution() #chooses highest res it can get
-        video = getBestVideo(yt)
-        audio = getBestAudio(yt)
-    else:
-        stream = yt.streams.get_lowest_resolution() #chooses lowest res it can get
+    video = getVideo(yt, resolution)
+    audio = getAudio(yt, bitrate)
 
     try:
-        video.download(output_path="working-files", filename="video.mp4") #downloads video
-        audio.download(output_path="working-files", filename="audio.mp4") #downloads audio
+        video.download(output_path=temp_path, filename="video.mp4") #downloads video
+        audio.download(output_path=temp_path, filename="audio.mp4") #downloads audio
 
-        input_video = ffmpeg.input("working-files/video.mp4")
-        input_audio = ffmpeg.input("working-files/audio.mp4")
-        output_file = SAVE_PATH + "/" + video.title + ".mp4"
+        file_name = video.title + ".mp4"
+        input_video = ffmpeg.input(temp_path + "/video.mp4")
+        input_audio = ffmpeg.input(temp_path + "/audio.mp4")
+        output_file = save_path + "/" + file_name
 
         ffmpeg.output(input_video, input_audio, output_file, vcodec="copy", acodec="copy").run(overwrite_output=True)
 
     except ffmpeg.Error as e:
-        print("Fuck")
+        print("Error merging audio and video files!!!")
+        return ["Error merging audio and video files!!! Please try again."]
     except FileNotFoundError:
         print("Error: ffmpeg not found. Ensure ffmpeg is installed and in your system's PATH.")
+        return ["Unknown ffmpeg Error!?!?!"]
 
     except:
         print("Some Error!")
+        return ["Unknown Error!?!?!"]
+    
+    os.remove(temp_path + "/video.mp4")
+    os.remove(temp_path + "/audio.mp4")
+    os.rmdir(temp_path)
+
     print('Task Completed!')
+    return [output_file, file_name]
 
-    return True
+def getVideo(yt, res):
+    if res == "best":
+        streams = yt.streams.filter(adaptive=True, file_extension='mp4', type='video')
+        if len(streams) > 0:
+            return streams[0]
+    elif res == "worst":
+        streams = yt.streams.filter(adaptive=True, file_extension='mp4', type='video')
+        if len(streams) > 0:
+            return streams[-1]
+    else:
+        streams = yt.streams.filter(adaptive=True, file_extension='mp4', type='video', res=res)
+        if len(streams) > 0:
+            return streams[0]
+        
+    return None
 
-def getBestVideo(yt):
-    #yt = YouTube("https://www.youtube.com/watch?v=MPBVyqdBgLM")
+def getAudio(yt, abr):
+    if abr == "best":
+        streams = yt.streams.filter(adaptive=True, file_extension='mp4', type='audio')
+        if len(streams) > 0:
+            return streams[-1]
+    elif abr == "worst":
+        streams = yt.streams.filter(adaptive=True, file_extension='mp4', type='audio')
+        if len(streams) > 0:
+            return streams[0]
+    else:
+        streams = yt.streams.filter(adaptive=True, file_extension='mp4', type='audio', abr=abr)
+        if len(streams) > 0:
+            return streams[0]
+        
+    return None
 
-    return yt.streams.filter(adaptive=True, file_extension='mp4', type='video')[0]
 
-def getBestAudio(yt):
-    #yt = YouTube("https://www.youtube.com/watch?v=MPBVyqdBgLM")
-
-    return yt.streams.filter(adaptive=True, file_extension='mp4', type='audio')[-1]
-
-
-
-def AudioDownload(link="https://www.youtube.com/watch?v=OpelfZhK2N8", savePath="C:/Users/gmeis/Downloads"):
-
-    SAVE_PATH = savePath #where it saves
-
+def AudioDownload(link="https://www.youtube.com/watch?v=OpelfZhK2N8", bitrate = "worst", save_path="C:/Users/gmeis/Downloads"):
     #link = "https://www.youtube.com/playlist?list=PL5H87eryjA0B5vi_nQA5fMXyk1QE_eRnz"
 
     if "playlist?list" in link:
@@ -71,11 +101,11 @@ def AudioDownload(link="https://www.youtube.com/watch?v=OpelfZhK2N8", savePath="
         filenames = [playlist_name]
 
         for yt in pl.videos:
-            audio = getBestAudio(yt)
+            audio = getAudio(yt, bitrate)
             filename = audio.title
 
             try:
-                audio.download(output_path=SAVE_PATH,filename=filename + '.mp3')
+                audio.download(output_path=save_path,filename=filename + '.mp3')
                 print('downloaded ' + filename + '.mp3')
             except:
                 print("Some Error!")
@@ -94,11 +124,11 @@ def AudioDownload(link="https://www.youtube.com/watch?v=OpelfZhK2N8", savePath="
             print("Connection Error") #to handle exception
             return None
 
-        audio = getBestAudio(yt) #chooses highest bitrate audio
+        audio = getAudio(yt, bitrate) #chooses highest bitrate audio
         filename = audio.title
 
         try:
-            audio.download(output_path=SAVE_PATH,filename=filename + '.mp3') #downloads file
+            audio.download(output_path=save_path,filename=filename + '.mp3') #downloads file
         except:
             print("Some Error!")
             return None
@@ -107,8 +137,6 @@ def AudioDownload(link="https://www.youtube.com/watch?v=OpelfZhK2N8", savePath="
         print('downloaded ' + filename + '.mp3')
         return filename
 
-#Download(highResolution=True)
-#AudioDownload()
 
-#getBestVideo(YouTube("https://www.youtube.com/watch?v=OpelfZhK2N8"))
-getBestAudio(YouTube("https://www.youtube.com/watch?v=MPBVyqdBgLM"))
+
+#Download(save_path="working-files/test", resolution="720p", bitrate="best")
--- app.py
+++ app.py
@@ -1,10 +1,71 @@
-from flask import Flask, render_template, url_for
+from key import SECRET_KEY
+from flask import Flask, render_template, url_for, request, send_file, session
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+import uuid
+from VideoDownloader import Download
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = SECRET_KEY
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+def get_guest_uuid():
+    if 'guest_uuid' not in session:
+        session['guest_uuid'] = str(uuid.uuid4())
+    return session['guest_uuid']
+
+class Save(db.Model):
+    id = db.Column(db.String(200), primary_key=True)
+    file_name = db.Column(db.String(200), nullable=False)
+    file_path = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    guest_id = get_guest_uuid()
+
+    if request.method == 'POST':
+        if "download" in request.form:
+            entry = db.session.get(Save, guest_id)
+            if entry is None:
+                return render_template('index.html', is_done=True, failed=True, message="Something went very worng, you should never see this message!")
+            
+            return send_file(entry.file_path, download_name=entry.file_name, as_attachment=True)
+        
+        else:
+            text = request.form['link']
+            res = request.form['resolution']
+            bitrate = request.form['audioq']
+
+            result = Download(text, res, bitrate, "working-files/" + str(guest_id))
+
+            if os.path.isfile(result[0]):
+                new_entry = Save(id=guest_id, file_path=result[0], file_name=result[1])
+                db.session.add(new_entry)
+                db.session.commit()
+
+                return render_template('index.html', is_done=True, failed=False, message=f"{result[1]} is ready to download")
+            else:
+                return render_template('index.html', is_done=True, failed=True, message=result[0])
+        
+    else:
+        return render_template('index.html', is_done=False)
+    
+@app.route('/test')
+def test():
+    return send_file("working-files/video.mp4", download_name="test.mp4", as_attachment=True)
+
+@app.route('/uuidtest')
+def uuidtest():
+    guest_id = get_guest_uuid()
+    return f"Hello, Guest! Your UUID is: {guest_id}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
